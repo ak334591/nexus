14b3ebb556fc52f44c6a4c273957145d036ff0de
AK334591 <AK334591@D-184003786.wipro.com>
2016-09-12 12:26:39 +0530

    changed 2

--- Sample/src/com/project/hello2.java
+++ Sample/src/com/project/hello2.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class hello2 {
 	 public static void main(String args[])
 	   {
-	      int x, y, z;
+	      int a, y, z;
 	      System.out.println("Enter two integers to calculate their sum ");
 	      Scanner in = new Scanner(System.in);
-	      x = in.nextInt();
+	      a = in.nextInt();
 	      y = in.nextInt();
-	      z = x + y;
+	      z = a + y;
 	      System.out.println("Sum of entered integers = "+z);
 	   }
 	}
